5ffb4600973c4fb15c0f7a47b8e38af42a590af9
hikari <mps.parsa.mps@gmail.com>
2023-08-21 23:23:08 +0330

    fix bug

--- src/Welcome.py
+++ src/Welcome.py
@@ -83,7 +83,7 @@ class Main(Quick):
         super(Main, self).__init__()
 
         # Load Main QML file
-        self.load(":ParchLinux.qml")
+        self.load(":/ParchLinux.qml")
 
         self.b1 = self.findChild("b1")  # Find Button 1
         self.b1.clicked.connect(self.b1_)  # Action: Connect Button 1 action to b1_
@@ -119,7 +119,7 @@ if __name__ == "__main__":
     # Start Qt Qui Application loop
     app = QGuiApplication([])
     # Set Qt Window Icon
-    app.setWindowIcon(QIcon(":ParchLogo.svg"))
+    app.setWindowIcon(QIcon(":/ParchLogo.svg"))
     # Loading Main Class
     m = Main()
     # Set loop exec
