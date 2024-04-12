f35077f109a0ad55e7c2753b8006289b9d36c46d
Sohrab Behdani <72007820+behdanisohrab@users.noreply.github.com>
2024-04-12 20:44:01 +0330

    New Parch Welcome written in slint and rust (#16)
    * new slint build
    
    * new slint build + desktop files
    
    * fixed the pkgbuild

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use std::process::Command;
+
+use slint::ComponentHandle;
+slint::include_modules!();
+
+fn main() {
+    let appwindow = App::new().unwrap();
+    appwindow.global::<Globals>().on_close(move || {
+        std::process::exit(0);
+    });
+    appwindow.global::<Globals>().on_openUrl(move |url| {
+        let _ = open::that(url.as_str());
+    });
+    appwindow.global::<Globals>().on_startInstaller(move || {
+        let _ = Command::new("sudo").arg("-E").arg("calamares").spawn();
+    });
+    let _ = appwindow.run();
+}
